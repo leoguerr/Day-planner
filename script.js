542dde90f00d3a0d599af6dbdf770bfd0f727342
Leo Guerrero <laoguerr@gmail.com>
2022-02-03 13:10:20 -0600

    updating JS file for updating time using moment

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-//variable for moment 
-var time = document.getElementById('currentDay');
-//Variables for each hour 9am-5pm
-var hour9 = document.getElementById('hour-9');
-var hour10 = document.getElementById('hour-10');
-var hour11 = document.getElementById('hour-11');
-var hour12 = document.getElementById('hour-12');
-var hour13 = document.getElementById('hour-13');
-var hour14 = document.getElementById('hour-14');
-var hour15 = document.getElementById('hour-15');
-var hour16 = document.getElementById('hour-16');
-var hour17 = document.getElementById('hour-17');
-
-//Current time 
+//Current time to display at #currentDay
 $("#currentDay").text(moment().format("LLL"));
+
+//function to set background of entry based on moment.js
+function updateTime() {
+    //selecting each "time-block" using jQuery
+    $(".time-block").each(function() {
+    //ParseInt to replace hour- for each "id" in block
+      var blockHour = parseInt($(this).attr("id").replace("hour-", ""));
+    //fetching current hour from moment  
+      var currentHour = parseInt(moment().format("Hour"));
+    //removing class when needed
+      $(this).removeClass("past present future");
+    //if else statement to change class based on currentHour
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour > currentHour) {
+        $(this).addClass("future");
+      } else {
+        $(this).addClass("present");
+      }
+    });
+  }
+//calling function 
+updateTime();
+
+//Local storage to save entries 
+
